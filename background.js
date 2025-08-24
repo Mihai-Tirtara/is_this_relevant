625cda648c6cc071524efb84437dc3df50d3f444
Mihai <mihai.tirtara@gmail.com>
2025-08-24 17:29:39 +0200

    Code clean-up and switch to only chrome namespace as it works on most browsers

--- background.js
+++ background.js
@@ -1,16 +1,9 @@
 // Listen for messages from content scripts
-if (typeof browser !== 'undefined') {
-  // Firefox
-  browser.runtime.onMessage.addListener((message, sender) => {
-    if (message.action === 'closeTab') {
-      browser.tabs.remove(sender.tab.id);
-    }
-  });
-} else if (typeof chrome !== 'undefined') {
-  // Chrome (for compatibility)
+if (typeof chrome !== 'undefined') {
   chrome.runtime.onMessage.addListener((message, sender) => {
     if (message.action === 'closeTab') {
       chrome.tabs.remove(sender.tab.id);
     }
   });
-}
+} 
+
